e9e6833beb0697c1f6b83067249ea0d5825ead2e
fjradi <fjradi96@gmail.com>
2024-07-06 00:14:52 +0700

    chore: add storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,8 +5,9 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "!.storybook"],
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
